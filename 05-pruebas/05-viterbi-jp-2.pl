bf7976c590db70df5d1c711c022f80fa8e1ea775
Juan Jose Morales <juan.morales01@epn.edu.ec>
2021-12-07 19:16:51 -0500

    alimentación de variables Viterbi por archivos

--- 05-pruebas/05-viterbi-jp-2.pl
+++ 05-pruebas/05-viterbi-jp-2.pl
@@ -2,29 +2,73 @@ use Algorithm::Viterbi;
 use Data::Dumper;
 use strict;
 use 5.28.1;
- 
-my $start_probability = { 'セン'=> 0.01, '<s>' => 0.9, 'トラル' => 0.01, '</s>' => 0.01};
 
-my $transition_probability = {
- '</s>' => {'セン'=> 0, '<s>' => 0, 'トラル' => 0, '</s>' => 1},
- 'トラル' => {'セン'=> 0.01, '<s>' => 0, 'トラル' => 0.01, '</s>' => 0.9},
- 'セン' => {'セン'=> 0.01, '<s>' => 0, 'トラル' => 0.9, '</s>' => 0.01},
- '<s>' => {'セン'=> 0.99, '<s>' => 0, 'トラル' => 0.01, '</s>' => 0}
-};
+my $file_in = '..\Documentos\simulador\vector_inicial.csv';
+my $file_emi = '..\Documentos\simulador\probabilidades_emision.csv';
+my $file_tra = '..\Documentos\simulador\probabilidades_transicion.csv';
 
-my $emission_probability = {
- '</s>' => {'セン'=> '0.01', '<s>' => '0', 'トラル' => '0.01', '</s>' => '0.97'}, 
- '`s_eh_n' => {'セン'=> '0.97', '<s>' => '0', 'トラル' => '0.01', '</s>' => '0.01'},
- 't_r_ax_l' => { 'セン' => '0.01', '<s>' => '0', 'トラル' => '0.97', '</s>' => '0.01'},
- '<s>' => {'セン'=> '0.01', '<s>' => '0.97', 'トラル' => '0.01', '</s>' => '0.01'} 
-};
+my %vector_inicial;
+my %probabilidad_emision;
+my %probabilidad_transicion;
+
+open(FHI, '<', $file_in) or die $!;
+open(FHE, '<', $file_emi) or die $!;
+open(FHT, '<', $file_tra) or die $!;
+
+while (<FHI>) {
+    my $linea = $_;
+    chomp($linea);
+    
+    #print "linea: $linea \n";
+    my @vector = split(/,/, $linea, length($linea));
+    
+    #print "parte 0: $vector[0] \n";
+    #print "parte 1: $vector[1] \n\n";
+    $vector_inicial{$vector[0]} = $vector[1];
+}
+
+while (<FHE>) {
+    my $linea = $_;
+    chomp($linea);
+    
+    my @emision = split(/,/, $linea, length($linea));
+    
+    $probabilidad_emision{$emision[0]}{$emision[1]} = $emision[2];
+}
+
+while (<FHT>) {
+    my $linea = $_;
+    chomp($linea);
+    
+    my @transicion = split(/,/, $linea, length($linea));
+    
+    $probabilidad_transicion{$transicion[0]}{$transicion[1]} = $transicion[2];
+}
+
+#################### VITERBI ####################
+
+print "\nDumper Vector Inicial\n";
+print Dumper(\%vector_inicial);
+
+print "\nDumper Probabilidad Emision\n";
+print Dumper(\%probabilidad_emision);
+
+print "\nDumper Probabilidad Transicion\n";
+print Dumper(\%probabilidad_transicion);
 
 
 my $v = Algorithm::Viterbi->new();
-$v->start($start_probability);
-$v->transition($transition_probability);
-$v->emission($emission_probability);
+$v->start(\%vector_inicial);
+$v->transition(\%probabilidad_transicion);
+$v->emission(\%probabilidad_emision);
+
  
 my $observations = [ '<s>', '`s_eh_n', 't_r_ax_l', '</s>' ];
 
+print "\nDumper Forward Viterbi\n";
 print Dumper ($v->forward_viterbi($observations));
+
+
+close(FHI);
+close(FHE);
+close(FHT);
