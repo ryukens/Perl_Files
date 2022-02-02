c6999ed01bee0b199b7416c21dc645bbaae1f316
Juan Jose Morales <juan.morales01@epn.edu.ec>
2022-02-02 16:42:53 -0500

    Primeras pruebas similitud e igualdad de katakanas

--- 05-pruebas/05-viterbi-jp-2.pl
+++ 05-pruebas/05-viterbi-jp-2.pl
@@ -8,6 +8,10 @@ use 5.28.1;
 my $file_in = '..\Documentos\simulador\vector_inicial.csv';
 my $file_emi = '..\Documentos\simulador\probabilidades_emision.csv';
 my $file_tra = '..\Documentos\simulador\probabilidades_transicion.csv';
+#my $file_obs = '..\Documentos\pruebas\Fonemas_Entrenamiento.txt';
+my $file_sal = '..\Documentos\pruebas\salida.txt';
+
+my $file_obs = '..\Documentos\pruebas\testeo.txt';
 
 my %vector_inicial;
 my %probabilidad_emision;
@@ -16,6 +20,8 @@ my %probabilidad_transicion;
 open(FHI, '<', $file_in) or die $!;
 open(FHE, '<', $file_emi) or die $!;
 open(FHT, '<', $file_tra) or die $!;
+open(FHO, '<', $file_obs) or die $!;
+open(FHS, '+>', $file_sal) or die $!;
 
 while (<FHI>) {
     my $linea = $_;
@@ -64,24 +70,49 @@ $v->emission(\%probabilidad_emision);
 
 #my $observations = [ '<s>', '`t_r_ey_l', '</s>'];
 #my $observations = [ '<s>', 's_t_er', '</s>'];
-my $observations = [ '<s>', 'eh_n', '`k_ae_p', 's_ax', '`l_ey', 'sh_ax_n','</s>'];
+#my $observations = [ '<s>', 'eh_n', '`k_ae_p', 's_ax', '`l_ey', 'sh_ax_n','</s>'];
 
 #my $observations = [ '<s>', '`s_ih_ng', 'k_r_ax','`n_ay', 'z_er', '</s>'];
 
 #my $observations = [ '<s>', '`k_aa_t', '</s>'];
 #my $observations = [ '`k_aa_t', '</s>'];
 
-print "\nDumper Forward Viterbi\n";
-#print Dumper ($v->forward_viterbi($observations));
+my $contador = 0;
 
-my ($prob_max, @path) = $v->forward_viterbi($observations);
+while (<FHO>) {
+    $contador += 1;
+    my $linea = $_;
+    chomp($linea);
+    my @observacion = split(/ /, $linea, length($linea));
+    
+    #print "\nDumper Forward Viterbi\n";
+    #print "@observacion\n";
+    my ($prob_max, @path) = $v->forward_viterbi(\@observacion);
 
-print "\nPROB_MAX: $prob_max\n";
-foreach my $answer (@path){
-    print "$answer ";    
+    #print "\nPROB_MAX: $prob_max\n";
+    #print "@path\n";
+    
+    print FHS "@path\n";
 }
 
+print "\ncontador: $contador";
+
+
+
+#print "\nDumper Forward Viterbi\n";
+#print Dumper ($v->forward_viterbi($observations));
+
+#my $observations = [ '<s>', '`l_ih_m', '`b_ao_z', '`d_ae_m', 's_ax_l', '`f_ih_sh', '</s>'];
+#
+#my ($prob_max, @path) = $v->forward_viterbi($observations);
+#
+#print "\nPROB_MAX: $prob_max\n";
+#foreach my $answer (@path){
+#    print "$answer ";    
+#}
 
 close(FHI);
 close(FHE);
 close(FHT);
+close(FHO);
+close(FHS);
